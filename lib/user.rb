49ecc363c30968ad8a9c2136cf210471b8d4bec7
KingHenryJr <henryyihengzhang@gmail.com>
2018-03-28 16:03:27 -0400

    added more user methods

--- lib/user.rb
+++ lib/user.rb
@@ -3,19 +3,35 @@ class User < ActiveRecord::Base
   has_many :workouts, through: :favorites
 
 
-  def see_workouts
-    self.workouts
+  def see_favorite_workouts
+    self.workouts.each_with_index do |workouts, index|
+      puts "#{index + 1}." + workouts.name
+    end
+    return nil
   end
 
-  def add_workout(workout_name)
-  found_name = Workout.find_by name: workout_name
-    self.workouts << found_name
+  def find_workout_id(workout_name)
+    found_name 
+
+    end
   end
+
+  def see_favorite_workout_exercieses(workout_name)
+    id = find_workout_id(workout_name)
+    Workout.id.select do |workout|
+      workout.
+
+  end
+
+
+  def find_workout(workout_name)
+    found_workout = Workout.find_by name: workout_name
+  end
+
 
   def add_workout_to_favorites(workout_name)
   found_name = Workout.find_by name: workout_name
-    self.favorites << found_name
-     binding.pry
+    self.workouts << found_name
   end
 
   def self.create_user(first_name, last_name, weight)
