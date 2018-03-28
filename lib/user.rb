736127120434ce33cce25cfa395b87f1c1fed20e
KingHenryJr <henryyihengzhang@gmail.com>
2018-03-28 16:36:37 -0400

    added methods

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :workouts, through: :favorites
 
-
+  #puts list of user's favorite workouts
   def see_favorite_workouts
     self.workouts.each_with_index do |workouts, index|
       puts "#{index + 1}." + workouts.name
@@ -10,30 +10,33 @@ class User < ActiveRecord::Base
     return nil
   end
 
+  #finds workout_id when inputting a workout_name
   def find_workout_id(workout_name)
-    found_name 
 
+  end
+
+  #views exercises of a given workout
+  def see_favorite_workout_exercises(workout_name)
+    all_exercises = find_workout(workout_name).exercises
+
+    all_exercises.each_with_index do |exercises, index|
+      puts "#{index + 1}." + exercises.name
     end
+    return nil
   end
 
-  def see_favorite_workout_exercieses(workout_name)
-    id = find_workout_id(workout_name)
-    Workout.id.select do |workout|
-      workout.
-
-  end
-
-
+  #finds a specific workout
   def find_workout(workout_name)
     found_workout = Workout.find_by name: workout_name
   end
 
-
+  #adds new workout to favorites
   def add_workout_to_favorites(workout_name)
   found_name = Workout.find_by name: workout_name
     self.workouts << found_name
   end
 
+  #creates new user
   def self.create_user(first_name, last_name, weight)
     User.find_or_create_by(first_name: first_name, last_name: last_name, weight: weight)
   end
