9d3020e4f403bf6610e7dae53020f158e893c2e9
rogercodes1 <roger.ucsb@gmail.com>
2018-03-29 09:34:06 -0400

    upated info.

--- lib/cli.rb
+++ lib/cli.rb
@@ -26,11 +26,14 @@ def new_or_returning(choice)
     if choice.downcase == "n"
       new_user
     elsif choice.downcase == "r"
-      print "Welcome back"
+      puts "Welcome back! Please provide your id"
+      puts "if you can't rememeber your id, please provide your first name."
+      r_input = gets.chomp
     elsif choice.downcase == "e"
-      print "Thank you for stopping by. Goodbye!"
+      puts "Thank you for stopping by. Goodbye!"
+      break
     else
-      print "Not a valid input. Please try again."
+      puts "Not a valid input. Please try again."
     end
     choice = gets.chomp
   end
@@ -40,7 +43,22 @@ end
 #give returning users options access
 #give new users option to create new
 #end if they pressed e
+def returning_user(r_input)
+  if r_input == r_input.to_i
+    user = User.find(r_input)
+    print " Great to have you back #{user.first_name} #{user.last_name}"
+  elsif r_input == r_input.to_s
+    user = User.find_by(first_name: r_input)
+    puts "Please provide your last name #{user}."
+    last = gets.chomp
+    full = User.find_by(first_name: r_input, last_name: last)
+    puts "Great to have you back #{full.first_name} #{full.last_name}"
+  else
+    puts "That is not a valid option."
 
+  end
+
+end
 def new_user
 
   puts "Welcome New User! What is your first name?"
@@ -58,12 +76,28 @@ def new_user
 
 end
 
+def options_screen
+  stars
+  puts "What would you like to do?"
+  puts "Please choose from the following options: "
+  puts "1. See current workout programs."
+  puts "2. Se list of exercises by category."
+  puts "3. Review your Favorite workouts."
+  puts "4. Add a Workout to your Favorites."
+  puts "5. Drop/Delete a Workout from your favorites"
+  puts "6. Create your own Workout Program."
+  puts "7. Add exercises to your Workout."
+  stars
+
+end
 
 # def run
+options_screen
   welcome
   input = gets.chomp
   new_or_returning(input)
-  new_user
+  # new_user
+  # returning_user
 
 # end
 
